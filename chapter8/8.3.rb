13039fe11e60000b31f58d2f5e31f8acd15fc7e0
Jae <jlee@yetitrails.com>
2012-07-16 13:24:03 +0100

    longest common sequence by dynamic programming

--- chapter8/8.3.rb
+++ chapter8/8.3.rb
@@ -1,11 +1,59 @@
 require "test/unit"
 
-def longest_common_substring(text1, text2)
-  ""
+def edit_cost(operation, current = "", target = "")
+  case
+    when operation == :match && [current.size, target.size] == [1, 1]
+      target == current ? 0 : Float::INFINITY
+    when operation == :insert && [current.size, target.size] == [0, 1]
+      1
+    when operation == :delete && [current.size, target.size] == [1, 0]
+      1
+    else
+      raise "unknown operation '#{operation}' from '#{current}' to '#{target}'"    
+  end
 end
 
-class TestEditDistance < Test::Unit::TestCase
-  def test_longest_common_substring
-    assert_equal("ograph", longest_common_substring("photograph", "tomography"))
+def longest_common_sequence(text1, text2)
+  editions = Array.new(text1.size+1) {[]} # editions[i][j] = [min_cost, [operations]] done to match ith text1 with jth text2
+  (0..text1.size).each do |text1_index|
+    editions[text1_index][0] = [text1_index, text1_index.times.map {|_| :delete}]
+  end
+  (0..text2.size).each do |text2_index|
+    editions[0][text2_index] = [text2_index, text2_index.times.map {|_| :insert}]
+  end
+
+  (1..text1.size).each do |text1_index|
+    (1..text2.size).each do |text2_index|
+      operations = [
+        [:match, text1[text1_index-1], text2[text2_index-1], editions[text1_index-1][text2_index-1]],
+        [:insert, "", text2[text2_index-1], editions[text1_index][text2_index-1]],
+        [:delete, text1[text1_index-1], "", editions[text1_index-1][text2_index]]
+      ]
+      
+      editions[text1_index][text2_index] = operations.map do |(operation, current, target, past_editions)|
+        cost = edit_cost(operation, current, target) + past_editions[0]
+        [cost, past_editions[1] + [operation]]
+      end.min_by {|edition| edition[0]}
+    end
+  end
+  
+  matches = [""]
+  editions[text1.size][text2.size][1].each_with_index do |operation, index|
+    if operation == :insert
+      text1.insert(index, "*")
+    end
+    
+    if operation == :match
+      matches[-1] = matches[-1] + text1[index]
+    else
+      matches << ""
+    end
+  end
+  matches.max_by {|match| match.size}
+end
+
+class TestLongestCommonSequence < Test::Unit::TestCase
+  def test_longest_common_sequence
+    assert_equal("ograph", longest_common_sequence("photograph", "tomography"))
   end
 end
