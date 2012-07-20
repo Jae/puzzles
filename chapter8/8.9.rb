0a501a4570739cc26c3f97415560c90cf802dd98
Jae Lee <jlee@yetitrails.com>
2012-07-20 08:30:24 +0100

    knapsack problem

--- chapter8/8.9.rb
+++ chapter8/8.9.rb
@@ -1,11 +1,22 @@
 require "test/unit"
 
-def knapsack(target, numbers)
+def find_knapsack(amount, coins)
+  coins.select {|coin| coin <= amount}.each do |coin|
+    if amount - coin == 0
+      return [coin]
+    else
+      smaller_knapsack = find_knapsack(amount-coin, coins-[coin])
+      unless smaller_knapsack.empty?
+        return [coin] + smaller_knapsack
+      end
+    end
+  end
+  return []
 end
 
 class TestKnapsackProblem < Test::Unit::TestCase
   def test_knapsack
-    assert_equal([], knapsack(22, [1,2,5,9,10]))
-    assert_equal([], knapsack(23, [1,2,5,9,10]))
+    assert_equal([1, 2, 9, 10], find_knapsack(22, [1,2,5,9,10]))
+    assert_equal([], find_knapsack(23, [1,2,5,9,10]))
   end
 end
