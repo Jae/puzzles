26520c6bf4e523aa17c08e5d9074f1b812509d1d
Jae Lee <jlee@yetitrails.com>
2012-07-28 17:34:38 +0100

    dynamic solution to minimum split cost

--- chapter8/8.12.rb
+++ chapter8/8.12.rb
@@ -1,30 +1,36 @@
 require "test/unit"
 
 def dynamic_minimum_split_cost(split_positions, string_size)
-  splits = Array.new(string_size) {[]} # splits[i][j] = [min_split_cost, splits] to split substring of i size from jth index
-  (0..string_size).each do |i|
-    splits[i][0] = [0, []]
-  end
-  (0..split_positions.size).each do |j|
-    splits[0][j] = [0, []]
+  splits = Array.new(string_size+1) {[]} # splits[i][j] = [min_split_cost, splits] to split substring of size i from jth index
+  (0...string_size).each do |jth_index|
+    splits[0][jth_index] = [0, []]
+    splits[1][jth_index] = [0, []]
   end
   
-  (1..string_size).each do |i|
-    (1..split_positions.size).each do |j|
-      legal_splits = split_positions[0...j].select {|split_position| i > split_position}
-      if legal_splits.empty?
-        splits[i][j] = splits[i][legal_splits.size]
+  (2..string_size).each do |size_i|
+    (0..string_size).each do |jth_index|
+      legal_positions = split_positions.select do |split_position|
+        (jth_index < split_position) && (split_position < jth_index + size_i)
+      end
+      
+      if legal_positions.empty?
+        splits[size_i][jth_index] = [0, []]
+      elsif legal_positions.size == 1
+        splits[size_i][jth_index] = [size_i, legal_positions]
       else
-        
-        splits[i][j] = [
-          [splits[i][legal_splits.size-1][0]+i, splits[i][legal_splits.size-1][1] + [legal_splits.last]],
-          []
-        ].min_by {|(split_cost, _)| split_cost}
+        splits[size_i][jth_index] = legal_positions.map do |split_position|
+          part1 = splits[split_position - jth_index][jth_index]
+          part2 = splits[size_i + jth_index - split_position][split_position]
+
+          [
+            part1[0] + part2[0] + size_i, [split_position] + part1[1] + part2[1]
+          ]
+        end.min_by {|(split_cost, _)| split_cost}
       end
     end
   end
   
-  splits[string_size][split_positions.size]
+  splits[string_size][0]
 end
 
 def recursive_minimum_split_cost(split_positions, string_size, string_from_index = 0)
@@ -49,8 +55,13 @@ def recursive_minimum_split_cost(split_positions, string_size, string_from_index
 end
 
 class TestMinimumCostOfSplit < Test::Unit::TestCase
-  def test_minimum_split_cost
+  def test_recursive_minimum_split_cost
     assert_equal([37, [10, 3, 8]], recursive_minimum_split_cost([3, 8, 10], 20))
     assert_equal([132, [15, 8, 3, 10, 28, 21, 35, 31, 39]], recursive_minimum_split_cost([3, 8, 10, 15, 21, 28, 31, 35, 39], 40))
+  end
+  
+  def test_dynamic_minimum_split_cost
+    assert_equal([37, [10, 3, 8]], dynamic_minimum_split_cost([3, 8, 10], 20))
+    assert_equal([132, [15, 8, 3, 10, 28, 21, 35, 31, 39]], dynamic_minimum_split_cost([3, 8, 10, 15, 21, 28, 31, 35, 39], 40))
   end
 end
