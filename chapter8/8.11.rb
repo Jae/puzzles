606841a83351e9b8dbebb7bcc713977fabfa502a
Jae Lee <jlee@yetitrails.com>
2012-08-05 15:44:45 +0100

    max contiguous sum by dynamic programming

--- chapter8/8.11.rb
+++ chapter8/8.11.rb
@@ -1,24 +1,24 @@
 require "test/unit"
 
 def maximum_contiguous_sum(numbers)
-  maximum_contiguous_numbers = [0, []]
-  numbers.each_index do |i|
-    (0...numbers.size-i).each do |contiguous_size|
-      contiguous_sum = numbers[i..(contiguous_size+i)].inject(0) {|memo, number| memo + number}
-      if maximum_contiguous_numbers[0] < contiguous_sum
-        maximum_contiguous_numbers = [contiguous_sum, numbers[i..(contiguous_size+i)]]
-      elsif maximum_contiguous_numbers[0] == contiguous_sum && contiguous_size < maximum_contiguous_numbers[1].size
-        maximum_contiguous_numbers = [contiguous_sum, numbers[i..(contiguous_size+i)]]
-      end
-    end
-    numbers[i...numbers.size]
+  sum = [numbers[0], [0]]
+
+  (1...numbers.size).each do |i|
+    sums = []
+    sums << sum                                                                # leave the run
+    sums << [sum[0] + numbers[i], sum[1] + [i]] if sum[1].last == i-1          # continue the run
+    sums << [numbers[i], [i]]                                                  # start a new run
+    sums << [numbers[sum[1].first..i].inject(0) {|memo, number| memo += number}, (sum[1].first..i).to_a] unless sum[1].last == i-1 # bridge the run
+    sum = sums.max_by {|(sum,_)| sum}
   end
-  maximum_contiguous_numbers[1]
+
+  sum[1].map {|i| numbers[i]}
 end
 
 class TestMaximumContiguousSum < Test::Unit::TestCase
   def test_maximum_contiguous_sum
     assert_equal([4, 1], maximum_contiguous_sum([4, 1, -2, -3, 2]))
-    assert_equal([2, -1, 2, 3], maximum_contiguous_sum([4, 1, -2, -3, 2, -1, 2, 3]))
+    assert_equal([4, 1, -2, -3, 2, -1, 2, 3], maximum_contiguous_sum([4, 1, -2, -3, 2, -1, 2, 3]))
+    assert_equal([59, 26, -53, 58, 97], maximum_contiguous_sum([31, -41, 59, 26, -53, 58, 97, -93, -23, 84]))
   end
 end
