33bead6bbfc8707dee13258cdf90d39a63e63df7
Jae Lee <jlee@yetitrails.com>
2012-09-15 23:14:31 +0100

    topological sorting of DAG with cycle detection

---
+++ chapter5/5.2.rb
@@ -0,0 +1,36 @@
+require "test/unit"
+
+def dfs(graph, node, visited=[], processed=[])
+  visited << node
+  graph[node].sort.each do |neighbour|
+    p "cycle detected #{node} -> #{neighbour}, #{visited.inspect}" if visited.include? neighbour and !processed.include? neighbour
+    dfs(graph, neighbour, visited, processed) unless visited.include? neighbour
+  end
+  processed << node
+end
+
+def topological_sort(graph)
+  visited = []
+  processed = []
+  graph.keys.each do |root|
+    dfs(graph, root, visited, processed) unless visited.include? root
+  end
+  processed.reverse
+end
+
+class TestTopologicalSort < Test::Unit::TestCase
+  def test_topological_sort
+    assert_equal([:A, :B, :D, :E, :C, :F, :H, :G, :I, :J], topological_sort({
+      A:[:B, :D], 
+      B:[:C, :D, :E], 
+      C:[:F], 
+      D:[:E, :G],
+      E:[:C, :F, :G],
+      F:[:H],
+      G:[:F, :I],
+      H:[:G, :J],
+      I:[:J],
+      J:[]
+    }))
+  end
+end
